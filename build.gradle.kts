bbae8b2bf1b3b6e55ffbf3cb095a4c00b924dc5c
jongmin92 <kdhx92@gmail.com>
2021-08-20 16:27:32 +0900

    gradle build-recipe-plugin 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,43 @@
+import com.linecorp.support.project.multi.recipe.configureByTypePrefix
+import com.linecorp.support.project.multi.recipe.configureByTypeSuffix
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
+    id("com.linecorp.build-recipe-plugin") version "1.1.1"
 }
 
-group = "com.jongmin.oss"
-version = "1.0-SNAPSHOT"
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 
-repositories {
-    mavenCentral()
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+configureByTypePrefix("kotlin") {
+    apply(plugin = "kotlin")
+
+    configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    dependencies {
+        api("org.jetbrains.kotlin:kotlin-reflect")
+        api("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    }
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+configureByTypeSuffix("lib") {
+    apply(plugin = "java-library")
 }
