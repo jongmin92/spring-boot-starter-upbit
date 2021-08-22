5b41a492139698e84c43556318d13219f9b39165
jongmin92 <kdhx92@gmail.com>
2021-08-22 15:42:15 +0900

    EXCHANGE API - 출금 - 출금 가능 정보 service interface 추가

--- service/src/main/kotlin/com/jongmin/upbit/exchange/withdraw/UpbitWithdrawsModel.kt
+++ service/src/main/kotlin/com/jongmin/upbit/exchange/withdraw/UpbitWithdrawsModel.kt
@@ -127,3 +127,199 @@ data class UpbitWithdraw(
      */
     val transactionType: String
 )
+
+data class UpbitWithdrawsChance(
+    /**
+     * 설명: 사용자의 보안등급 정보
+     * 타입: MemberLevel
+     */
+    val memberLevel: MemberLevel,
+
+    /**
+     * 설명: 화폐 정보
+     * 타입: Currency
+     */
+    val currency: Currency,
+
+    /**
+     * 설명: 사용자의 계좌 정보
+     * 타입: Account
+     */
+    val account: Account,
+
+    /**
+     * 설명: 출금 제약 정보
+     * 타입: WithdrawLimit
+     */
+    val withdrawLimit: WithdrawLimit
+) {
+    data class MemberLevel(
+        /**
+         * 설명: 사용자의 보안등급
+         * 타입: Integer
+         */
+        val securityLevel: Int,
+
+        /**
+         * 설명: 사용자의 수수료등급
+         * 타입: Integer
+         */
+        val feeLevel: Int,
+
+        /**
+         * 설명: 사용자의 이메일 인증 여부
+         * 타입: Boolean
+         */
+        val emailVerified: Boolean,
+
+        /**
+         * 설명: 사용자의 실명 인증 여부
+         * 타입: Boolean
+         */
+        val identityAuthVerified: Boolean,
+
+        /**
+         * 설명: 사용자의 계좌 인증 여부
+         * 타입: Boolean
+         */
+        val bankAccountVerified: Boolean,
+
+        /**
+         * 설명: 사용자의 카카오페이 인증 여부
+         * 타입: Boolean
+         */
+        val kakaoPayAuthVerified: Boolean,
+
+        /**
+         * 설명: 사용자의 계정 보호 상태
+         * 타입: Boolean
+         */
+        val locked: Boolean,
+
+        /**
+         * 설명: 사용자의 출금 보호 상태
+         * 타입: Boolean
+         */
+        val walletLocked: Boolean
+    )
+
+    data class Currency(
+        /**
+         * 설명: 화폐를 의미하는 영문 대문자 코드
+         * 타입: String
+         */
+        val code: String,
+
+        /**
+         * 설명: 해당 화폐의 출금 수수료
+         * 타입: NumberString
+         */
+        val withdrawFee: String,
+
+        /**
+         * 설명: 화폐의 코인 여부
+         * 타입: Boolean
+         */
+        val isCoin: Boolean,
+
+        /**
+         * 설명: 해당 화폐의 지갑 상태
+         * 타입: String
+         */
+        val walletState: String,
+
+        /**
+         * 설명: 해당 화폐가 지원하는 입출금 정보
+         * 타입: List<String>
+         */
+        val walletSupport: List<String>
+    )
+
+    data class Account(
+        /**
+         * 설명: 화폐를 의미하는 영문 대문자 코드
+         * 타입: String
+         */
+        val currency: String,
+
+        /**
+         * 설명: 주문가능 금액/수량
+         * 타입: NumberString
+         */
+        val balance: String,
+
+        /**
+         * 설명: 주문 중 묶여있는 금액/수량
+         * 타입: NumberString
+         */
+        val locked: String,
+
+        /**
+         * 설명: 매수평균가
+         * 타입: NumberString
+         */
+        val avgBuyPrice: String,
+
+        /**
+         * 설명: 매수평균가 수정 여부
+         * 타입: Boolean
+         */
+        val avgBuyPriceModified: Boolean,
+
+        /**
+         * 설명: 평단가 기준 화폐
+         * 타입: String
+         */
+        val unitCurrency: String
+    )
+
+    data class WithdrawLimit(
+        /**
+         * 설명: 화폐를 의미하는 영문 대문자 코드
+         * 타입: String
+         */
+        val currency: String,
+
+        /**
+         * 설명: 출금 최소 금액/수량
+         * 타입: NumberString
+         */
+        val minimum: String,
+
+        /**
+         * 설명: 1회 출금 한도
+         * 타입: NumberString
+         */
+        val onetime: String,
+
+        /**
+         * 설명: 1일 출금 한도
+         * 타입: NumberString
+         */
+        val daily: String,
+
+        /**
+         * 설명: 1일 잔여 출금 한도
+         * 타입: NumberString
+         */
+        val remainingDaily: String,
+
+        /**
+         * 설명: 통합 1일 잔여 출금 한도
+         * 타입: NumberString
+         */
+        val remainingDailyKrw: String,
+
+        /**
+         * 설명: 출금 금액/수량 소수점 자리 수
+         * 타입: Integer
+         */
+        val fixed: Int,
+
+        /**
+         * 설명: 출금 지원 여부
+         * 타입: Boolean
+         */
+        val canWithdraw: Boolean
+    )
+}
--- service/src/main/kotlin/com/jongmin/upbit/exchange/withdraw/UpbitWithdrawsService.kt
+++ service/src/main/kotlin/com/jongmin/upbit/exchange/withdraw/UpbitWithdrawsService.kt
@@ -26,7 +26,16 @@ interface UpbitWithdrawsService {
      * @param uuid 출금 UUID
      * @param txid 출금 TXID
      * @param currency Currency 코드
-     * @return
+     * @return 개별 출금 정보
      */
     fun getWithdraw(uuid: String, txid: String, currency: String): UpbitWithdraw
+
+    /**
+     * 출금 가능 정보
+     * 해당 통화의 가능한 출금 정보를 확인한다.
+     *
+     * @param currency Currency 코드
+     * @return 해당 통화의 가능한 출금 정보
+     */
+    fun getWithdrawsChance(currency: String): UpbitWithdrawsChance
 }
