154d55c326751df6f716cd93389b73e6640fe2a6
noveler17 <noveler17@gmail.com>
2021-09-19 11:51:42 +0900

    Bump up Kotlin, Kotlin Coroutines, and Kotlin-related plugins into the latest (#11)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,36 +4,35 @@ import com.linecorp.support.project.multi.recipe.configureByTypeSuffix
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
-    kotlin("kapt") version "1.5.21"
-    kotlin("plugin.spring") version "1.4.21" apply false
     id("com.linecorp.build-recipe-plugin") version "1.1.1"
+    kotlin("jvm") version "1.5.30"
+    kotlin("kapt") version "1.5.30"
+    kotlin("plugin.spring") version "1.5.30" apply false
 }
 
 allprojects {
     repositories {
         mavenCentral()
     }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
 }
 
 configureByTypePrefix("kotlin") {
-    apply(plugin = "kotlin")
-    apply(plugin = "org.jetbrains.kotlin.kapt")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
 
-    configure<JavaPluginExtension> {
-        sourceCompatibility = JavaVersion.VERSION_1_8
+    tasks {
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+
+        withType<Test> {
+            useJUnitPlatform()
+        }
     }
 
     dependencies {
         api(platform("org.springframework.boot:spring-boot-dependencies:2.5.4"))
+        api(platform("org.jetbrains.kotlin:kotlin-bom:1.5.30"))
+        api(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.5.2"))
 
         api("org.jetbrains.kotlin:kotlin-reflect")
         api("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -45,7 +44,8 @@ configureByTypePrefix("kotlin") {
 }
 
 configureByTypeHaving("boot") {
-    apply(plugin = "kotlin-spring")
+    apply(plugin = "org.jetbrains.kotlin.kapt")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 
     dependencies {
         kapt("org.springframework.boot:spring-boot-autoconfigure-processor:2.5.4")
